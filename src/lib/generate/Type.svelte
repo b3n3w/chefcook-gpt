ee10f6a964f68fbb1065b4f482f35a5daef1f21c
Benedikt Wagner <benedikt@wagner-labs.com>
2023-04-19 18:26:35 +0200

    refactor

---
+++ src/lib/generate/Type.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import IconMeat from '~icons/mdi/meat';
+	import IconFish from '~icons/mdi/fish';
+	import LucideVegan from '~icons/lucide/vegan';
+	import ElLeaf from '~icons/el/leaf';
+
+	export let select = false;
+</script>
+
+a
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div
+	class="shadow-sm w-16 h-24 sm:w-32 sm:h-24 rounded-xl hover:shadow-sm hover:shadow-purple-200 border-2 {select
+		? 'border-orange-400 border-3'
+		: ''}"
+	on:click={() => (select = !select)}
+>
+	<div class="grid content-center dark:text-white text-center justify-center">
+		<div class="flex justify-center pt-3 pb-3 sm:pb-2">
+			<IconMeat class="text-2xl sm:text-3xl md:text-4xl" />
+		</div>
+		<div class="pb-2 text-lg">Meat</div>
+	</div>
+</div>
--- src/routes/generate/+page.svelte
+++ src/routes/generate/+page.svelte
@@ -1,119 +1,19 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
-	import { fly } from 'svelte/transition';
-	import { Button } from 'flowbite-svelte';
+	import { fade } from 'svelte/transition';
 
-	import Types from './components/Types.svelte';
-	import Mains from './components/Mains.svelte';
-	import Ingredients from './components/Ingredients.svelte';
-
-	import { fetchRecipe } from '$lib/openai-api';
-	import { apikey, saveRecipe } from '$lib/shared/stores/general';
-	import { ingredientsStore } from '$lib/shared/stores/general';
-	import { prompts } from '$lib/prompts';
-
-	let veganSelect = false;
-	let fishSelect = false;
-	let meatSelect = false;
-	let veggieSelect = false;
-
-	let pastaSelect = false;
-	let riceSelect = false;
-
-	let generating = false;
-
-	let validAPI = true;
-
-	async function generatePromt() {
-		generating = true;
-
-		let main = pastaSelect ? 'pasta' : riceSelect ? 'rice' : '';
-		let type = meatSelect
-			? 'meat'
-			: veggieSelect
-			? 'veggie'
-			: veganSelect
-			? 'vegan'
-			: fishSelect
-			? 'fish'
-			: 'without';
-
-		let prompt = prompts.english[type];
-
-		const ingredientNames = $ingredientsStore.map((ingredient) => ingredient.ingredientName);
-
-		prompt = prompt.replace(/-MAIN-/, main);
-		prompt = prompt.replace(/-INGREDIENTS-/, ingredientNames.join(', '));
-
-		prompt += prompts.instructions;
-
-		console.log(prompt);
-		
-		const { status, content } = await fetchRecipe($apikey, prompt);
-
-		if (status == 200) {
-			validAPI = true;
-		} else {
-			validAPI = false;
-			return;
-		}
-
-		generating = false;
-		saveRecipe(content);
-		goto('/recipe');
-	}
 </script>
 
-<div class="stack">
-	<div class="flex flex-wrap justify-center mt-5 font-thin dark:text-white">
-		<p class="uppercase text-lg sm:text-xl">Select what you love</p>
+<!-- svelte-ignore a11y-click-events-have-key-events -->
+<div in:fade={{ delay: 50 }} class="flex justify-center space-x-4 flex-wrap h-screen place-items-center">
+	<div on:click={() => goto('/generate/hearty')} class="w-1/4 h-1/4 border-2 rounded-lg bg-slate-500">
+		<div class="flex justify-center">
+			<div>Hearty</div>
+		</div>
 	</div>
-	<Types
-		bind:vegan={veganSelect}
-		bind:veggie={veggieSelect}
-		bind:fish={fishSelect}
-		bind:meat={meatSelect}
-	/>
-	<div class="flex flex-wrap justify-center dark:text-white">
-		<p class="uppercase text-lg sm:text-xl font-thin">Select your main</p>
-	</div>
-	<Mains bind:pasta={pastaSelect} bind:rice={riceSelect} />
-	<div class="flex flex-wrap justify-center dark:text-white">
-		<p class="uppercase text-lg sm:text-xl font-thin">What do you have at home ?</p>
-	</div>
-	<Ingredients bind:ingredients={$ingredientsStore} />
-	<div class="flex-wrap grid justify-center">
-		{#if validAPI}
-			{#if generating}
-				<div class="font-thin dark:text-white text-sm">This takes around 15 seconds</div>
-				<Button disabled gradient color="cyanToBlue" size="lg">
-					<div class="flex items-center justify-center">
-						<div
-							class="border-t-transparent border-solid animate-spin rounded-full border-white border-4"
-						/>
-						<div class="ml-2">
-							Processing... <div />
-						</div>
-					</div>
-				</Button>
-			{:else}
-				<div in:fly={{ y: 50 }}>
-					<Button
-						class="btn-generate "
-						gradient
-						color="cyanToBlue"
-						size="lg"
-						on:click={() => generatePromt()}>Generate now</Button
-					>
-				</div>
-			{/if}
-		{:else}
-			<div class="justify-center">
-				<div class="font-light text-red-500 text-sm">Api-Key not valid or not set</div>
-				<Button class="btn-generate " href="/settings" gradient color="red" size="sm"
-					>Open settings</Button
-				>
-			</div>
-		{/if}
+	<div on:click={() => goto('/generate/sweet')} class="w-1/4 h-1/4 border-2 rounded-lg bg-slate-500">
+		<div class="flex justify-center">
+			<div>Sweet</div>
+		</div>
 	</div>
 </div>

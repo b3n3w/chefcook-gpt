c32a96870c562d33d7710044820cd5ac0002c2be
Benedikt Wagner <benedikt@wagner-labs.com>
2023-04-20 19:28:31 +0200

    refactor code

--- src/lib/openai-api.ts
+++ src/lib/openai-api.ts
@@ -3,46 +3,36 @@ import { get } from 'svelte/store';
 import { apikeyStore } from './shared/stores/general';
 
 export async function fetchRecipe(prompt: string) {
+	const apiKey = get(apikeyStore);
 
-	const apiKey = get(apikeyStore)
-
-	if (apiKey && await validateApiKey(apiKey)) {
-		const configuration = new Configuration({
-			apiKey: apiKey
-		});
-		const openai = new OpenAIApi(configuration);
-		const messages: any = []
-
-		messages.push({
-			role: 'user',
-			content: prompt
-		});
-		try {
-			let completion = await openai.createChatCompletion({
-				model: 'gpt-3.5-turbo',
-				messages: messages
-			});
-			return { status: 200, content: completion.data.choices[0].message.content }
-		} catch (error: any) {
-			if (error.response) {
-				return { status: error.response.status, content: "" }
-			}
-		}
-	} else {
-		console.log('API key is undefined or empty');
+	if (!apiKey || !(await validateApiKey(apiKey))) {
 		const response = await fetch('/api/generate', {
 			method: 'POST',
-			body: JSON.stringify({ prompt: prompt })
-		})
-		console.log(response);
-		
-		if (response.status != 200) {
-			return { status: 403, content: "" }
-		} else {
-			return { status: 200, content: response.content }
+			body: JSON.stringify({ prompt })
+		});
+		if (response.status !== 200) {
+			return { status: 401, content: '' };
 		}
+		const content = await response.json();
+		return { status: 200, content };
 	}
-	return { status: 400, content: "" }
+
+	const configuration = new Configuration({ apiKey });
+	const openai = new OpenAIApi(configuration);
+	const messages = [{ role: 'user', content: prompt }];
+
+	try {
+		const completion = await openai.createChatCompletion({
+			model: 'gpt-3.5-turbo',
+			messages
+		});
+		return { status: 200, content: completion.data.choices[0].message.content };
+	} catch (error: any) {
+		if (error.response) {
+			return { status: error.response.status, content: '' };
+		}
+		return { status: 400, content: '' };
+	}
 }
 
 export async function validateApiKey(apiKey: string): Promise<Boolean> {
---
+++ src/routes/api/generate/+server.ts
@@ -0,0 +1,43 @@
+import type { RequestHandler } from './$types';
+import { Configuration, OpenAIApi } from 'openai';
+import { OPENAI_API_KEY } from '$env/static/private';
+
+/**
+ * This method provides the free try-out mode for new users. 
+ * The ratelimit will be to get 2 recipes per month, per user.
+ * @returns generated recipe
+ */export const POST: RequestHandler = async ({ request }) => {
+    
+    // Parse request data
+    const data = await request.json();
+    const prompt = data.prompt;
+    if (!prompt) {
+        return new Response("No prompt provided", { status: 400 });
+    }
+
+    // Set up OpenAI API client
+    const configuration = new Configuration({ apiKey: OPENAI_API_KEY });
+    const openai = new OpenAIApi(configuration);
+
+    // Prepare message object
+    const messages = [{ role: 'user', content: prompt }];
+
+    try {
+        // Generate AI response
+        const completion = await openai.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages,
+        });
+
+        // Return response data
+        const content = completion.data.choices[0].message.content;
+        return new Response(JSON.stringify(content), { status: 200 });
+    } catch (error: any) {
+        // Handle errors from OpenAI API
+        if (error.response) {
+            const status = error.response.status;
+            return new Response(JSON.stringify({ status, content: "" }), { status: 400 });
+        }
+    }
+    return new Response("Something went wrong", { status: 500 });
+}

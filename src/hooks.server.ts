8bc79019679200a4f469db3964bcdf8e0752c613
Benedikt Wagenr <benedikt.wagner@stud-mail.uni-wuerzburg.de>
2023-04-21 18:23:44 +0200

    move i18n lib

---
+++ src/hooks.server.ts
@@ -0,0 +1,17 @@
+import { type Handle } from '@sveltejs/kit';
+import { sequence } from '@sveltejs/kit/hooks';
+import { initAcceptLanguageHeaderDetector } from 'typesafe-i18n/detectors';
+import { detectLocale } from '$lib/i18n/i18n-util.js';
+
+const handleDetectLocale = (async ({ event, resolve }) => {
+    
+	const acceptLanguageHeaderDetector = initAcceptLanguageHeaderDetector(event.request);
+	const locale = detectLocale(acceptLanguageHeaderDetector);
+	event.locals.locale = locale;
+
+	return resolve(event, { transformPageChunk: ({ html }) => html.replace('%lang%', locale) });
+}) satisfies Handle;
+
+
+
+export const handle = sequence(handleDetectLocale);

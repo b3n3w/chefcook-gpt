10d759a3719a9fb735e81f862b8008bd7c46ad94
Benedikt Wagenr <benedikt.wagner@stud-mail.uni-wuerzburg.de>
2023-04-21 20:41:44 +0200

    add translation for spanish, hungarian, italian, polish and russian

--- src/lib/i18n/it/index.ts
+++ src/lib/i18n/it/index.ts
@@ -2,7 +2,76 @@ import type { Translation } from '../i18n-types'
 import en from '../en';
 
 const it: Translation = {
-	...(en as Translation)
+	...(en as Translation),
+	common: {
+		button: "Genera uno!"
+	},
+	nav: {
+		create: "Creare",
+		recipes: "Ricette",
+		settings: "Impostazioni"
+	},
+	landing: {
+		header: {
+			gen: "creare",
+			recipe: "La tua ricetta",
+			ai: "Con AI"
+		},
+		idea: "Cibo a casa ma non ho idea di cosa cucinare"
+	},
+	generate: {
+		headers: {
+			type: "Seleziona ciò che ami",
+			main: "Seleziona il tuo principale",
+			atHome: "Cosa hai a casa?"
+		},
+		types: {
+			vegan: "Vegana",
+			veggie: "Vegetariana",
+			fish: "Pescare",
+			meat: "Carne"
+		},
+		mains: {
+			pasta: "Pasta",
+			rice: "Riso",
+			salad: "Insalata"
+		},
+		info: {
+			noIngredients: "Nessun ingrediente aggiunto",
+			inputPlaceholer: "Inserisci per aggiungere",
+			time: "Questo richiede circa 15 secondi",
+			invalidKey: "Api-key non valido o non impostato",
+			processing: "In lavorazione..."
+		},
+		buttons: {
+			settings: "Impostazioni aperte"
+		}
+	},
+	recipes: {
+		header: "Le tue ricette",
+		noRecipes: "Attualmente non hai alcuna ricetta 😰"
+	},
+	recipe: {
+		translate: "Tradurre",
+		ingredients: "ingredienti",
+		instructions: "Istruzioni"
+	},
+	settings: {
+		darkmode: {
+			header: "Modalità scura",
+			enabled: "disattivare",
+			disabled: "Abilitare"
+		},
+		apiKey: {
+			info: "Se non hai ancora un api-key. Si prega di registrarsi a:",
+			invalid: "Set non valido o nessun api-key!",
+			valid: "Api-key sembra buono! Cuciniamo!",
+			button: {
+				save: "Salva",
+				valid: "Valida"
+			}
+		}
+	}
 };
 
 export default it
--- src/lib/i18n/pl/index.ts
+++ src/lib/i18n/pl/index.ts
@@ -2,7 +2,76 @@ import type { Translation } from '../i18n-types'
 import en from '../en';
 
 const pl: Translation = {
-	...(en as Translation)
+	...(en as Translation),
+	common: {
+		button: "Wygeneruj jeden!"
+	},
+	nav: {
+		create: "Tworzyć",
+		recipes: "Przepisy",
+		settings: "Ustawienia"
+	},
+	landing: {
+		header: {
+			gen: "Generować",
+			recipe: "Twój przepis",
+			ai: "Z AI"
+		},
+		idea: "Jedzenie w domu, ale nie mam pojęcia, co gotować"
+	},
+	generate: {
+		headers: {
+			type: "Wybierz to, co kochasz",
+			main: "Wybierz swój główny",
+			atHome: "Co masz w domu?"
+		},
+		types: {
+			vegan: "Wegański",
+			veggie: "Wegetaria",
+			fish: "Ryba",
+			meat: "Mięso"
+		},
+		mains: {
+			pasta: "Makaron",
+			rice: "Ryż",
+			salad: "Sałatka"
+		},
+		info: {
+			noIngredients: "Nie dodano żadnych składników",
+			inputPlaceholer: "Enter, aby dodać",
+			time: "To zajmuje około 15 sekund",
+			invalidKey: "Api-Key nieprawidłowy lub nie ustawiony",
+			processing: "Przetwarzanie..."
+		},
+		buttons: {
+			settings: "Otwórz ustawienia"
+		}
+	},
+	recipes: {
+		header: "Twoje przepisy",
+		noRecipes: "Obecnie nie masz żadnego przepisu 😰"
+	},
+	recipe: {
+		translate: "Tłumaczyć",
+		ingredients: "Składniki",
+		instructions: "Instrukcje"
+	},
+	settings: {
+		darkmode: {
+			header: "Darkmode",
+			enabled: "Wyłączyć",
+			disabled: "Włączać"
+		},
+		apiKey: {
+			info: "Jeśli nie masz jeszcze klawisza API. Zarejestruj się na:",
+			invalid: "Nieprawidłowy lub brak zestawu API-klawisza!",
+			valid: "KLUKA API-KLEY wygląda dobrze! Gotujmy!",
+			button: {
+				save: "Ratować",
+				valid: "Ważny"
+			}
+		}
+	}
 };
 
 export default pl
